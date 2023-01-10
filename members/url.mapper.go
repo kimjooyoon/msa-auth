7b3422cb5517dd17d529467d9b16abb25aeeaf10
kimjooyoon <asketeddy@gmail.com>
2023-01-10 16:50:52 +0900

    feat(mapper): POST logout 추가

--- members/url.mapper.go
+++ members/url.mapper.go
@@ -5,6 +5,7 @@ import "github.com/gin-gonic/gin"
 func (r Controller) Mapping(server *gin.Engine) {
 	server.POST("sign-on", r.SignOn)
 	server.POST("sign-in", r.SignIn)
+	server.POST("logout", r.Logout)
 	server.GET("my/info", r.MyInfo)
 	server.GET("members", r.SearchUser)
 	server.GET("emails", r.SearchByEmail)
