028abaa2765b7ec79e0352209490da5522cba742
kimjooyoon <asketeddy@gmail.com>
2023-01-18 14:31:09 +0900

    feat(mapper): PUT my/info 추가

--- members/url.mapper.go
+++ members/url.mapper.go
@@ -6,6 +6,7 @@ func (r Controller) Mapping(server *gin.Engine) {
 	server.POST("sign-on", r.SignOn)
 	server.POST("sign-in", r.SignIn)
 	server.POST("logout", r.Logout)
+	server.PUT("my/info", r.MyInfoUpdate)
 	server.GET("my/info", r.MyInfo)
 	server.GET("members", r.SearchUser)
 	server.GET("emails", r.SearchByEmail)
